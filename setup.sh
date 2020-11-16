6ca864041d9962c968c484580bfc15c039e9035a
DeVeL_AnOnIk <27363518+anonik9900@users.noreply.github.com>
2020-11-16 00:38:15 +0000

    0.1 beta

---
+++ setup.sh
@@ -0,0 +1,19 @@
+sudo apt-get install python
+sudo apt-get install python2
+sudo apt-get install python3
+sudo apt update 
+sudo apt-get update
+sudo apt-get install python-pip
+sudo apt-get install python3-pip
+sudo apt-get install curl
+sudo apt update 
+sudo curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
+sudo pip install pyfiglet
+sudo pip3 install pyfiglet
+sudo pip install pytube3
+sudo pip install pytube
+sudo pip3 install pytube
+sudo apt update 
+sudo apt-get update
+
